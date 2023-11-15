adb7bc0c8c09e60c75edf4c77b0512d4829ced5f
raslaniks <61558635+raslaniks@users.noreply.github.com>
2023-11-15 11:16:11 +0200

    Update TestClass.py

--- tests/TestClass.py
+++ tests/TestClass.py
@@ -1,11 +1,10 @@
 import pytest
 import os
-class TestClassDemoInstance:
-    value = 0
-
-    def test_simple(self):
-        self.value = 1
-        assert self.value == 1
-
-    def test_multiboot(self):
+def test_multiboot(self):
         os.system("HOME_PATH/main.py -o tests/test1.elf tests/test1.hny")
+def test_hello_world(self):
+        os.system("HOME_PATH/main.py -o tests/test1.elf tests/test1.hny")
+
+
+
+    
