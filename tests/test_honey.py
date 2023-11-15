1f1405a4d80d86da3f92e1030cae6016f906210e
raslaniks <61558635+raslaniks@users.noreply.github.com>
2023-11-15 12:03:59 +0200

    Update test_honey.py

--- tests/test_honey.py
+++ tests/test_honey.py
@@ -1,10 +1,10 @@
 import pytest
 import os
-def test_compile_multiboot():
+def test_compile_hello_world():
         
         ret_code = os.system("HOME_PATH/main.py -o tests/test1.elf tests/test1.hny")
         assert ret_code == 0
-def test_compile_hello_world():
+def test_compile_multiboot():
         ret_code = os.system("HOME_PATH/main.py -o tests/test1.elf tests/test2.hny")
         assert ret_code == 0
 # def test_multiboot():
