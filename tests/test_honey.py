8a37eddf928480b762f953ffc7e12ef2414827cd
raslaniks <61558635+raslaniks@users.noreply.github.com>
2023-11-15 11:43:48 +0200

    Update test_honey.py

--- tests/test_honey.py
+++ tests/test_honey.py
@@ -3,7 +3,7 @@ import os
 def test_multiboot():
         os.system("HOME_PATH/main.py -o tests/test1.elf tests/test1.hny")
 def test_hello_world():
-        os.system("HOME_PATH/main.py -o tests/test1.elf tests/test1.hny")
+        os.system("HOME_PATH/main.py -o tests/test1.elf tests/test2.hny")
 
 
 
